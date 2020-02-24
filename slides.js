1f03e9be3f6cc609410edce087cc25ef6f8614e9
Joan Marie Verba <verba001@umn.edu>
2020-02-24 14:17:06 -0600

    refine popup

--- slides.js
+++ slides.js
@@ -89,11 +89,14 @@ function showDivs(n) {
 // popup code
 
 function onMouseOut(event) {
-    // Remove this event listener
-    document.removeEventListener("mouseout", onMouseOut);
+    // If the mouse is near the top of the window, show the popup
+    if (event.clientY < 50) {
+        // Remove this event listener
+        document.removeEventListener("mouseout", onMouseOut);
 
-    // Show the popup
-    document.getElementById("popup").style.display = "block";
+        // Show the popup
+        document.getElementById("popup").style.display = "block";
+    }
 }
 
 document.addEventListener("mouseout", onMouseOut);
